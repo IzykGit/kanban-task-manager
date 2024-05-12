64d3c11f272c34e297505bea3241484fa41e008c
Izyk <lance_hemphill1234@yahoo.com>
2024-05-11 21:34:21 -0400

    added sidebar to project

--- Frontend/src/pages/Home.js
+++ Frontend/src/pages/Home.js
@@ -1,10 +1,46 @@
 import React from 'react'
 import styles from '../styles/Home.module.css'
 
+import logodark from '../assets/logo-dark.svg'
+import board from '../assets/icon-board.svg'
+
 const Home = () => {
   return (
     <main>
+          <aside className={styles.board_menu}>
+                <div className={styles.logo_container}>
+                    <img src={logodark} alt='Kanban' className={styles.logo}/>
+                </div>
 
+
+                <div className={styles.boards_container}>
+                    <p>ALL BOARDS (3)</p>
+
+                    <div className={styles.board_active}>
+                        <img src={board} alt=''/>
+                        <button>Placeholder</button>
+                    </div>
+                    <div className={styles.board}>
+                        <img src={board} alt=''/>
+                        <button>Placeholder</button>
+                    </div>
+                    <div className={styles.board}>
+                        <img src={board} alt=''/>
+                        <button>Placeholder</button>
+                    </div>
+                    <div className={styles.board}>
+                        <img src={board} alt=''/>
+                        <button>Placeholder</button>
+                    </div>
+                    
+
+                    <div className={styles.create_board}>
+                        <img src={board} alt=''/>
+                        <button>+ Create Board</button>
+                    </div>
+
+                </div>
+          </aside>
     </main>
   )
 }
